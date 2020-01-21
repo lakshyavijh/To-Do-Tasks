343b9b04f03ed8dc8f37bdf3d660f7171c83f705
Lakshya <lakshya.vijh99@gmail.com>
2020-01-21 16:12:37 +0530

    mera

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ let sanitizeHTML=require('sanitize-html')
 
 let app = express()
 let db
-let port = process.env.port
+let port = process.env.PORT
 if(port == null || port == ""){
-  post=3000
+  port=3000
 }
 app.use(express.static('public'))
 

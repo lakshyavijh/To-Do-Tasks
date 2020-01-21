62d51b3a35b92d2552a9486f28bc8fce9de49a93
Lakshya <lakshya.vijh99@gmail.com>
2020-01-21 15:46:44 +0530

    Our first commit

---
+++ public/browser.js
@@ -0,0 +1,57 @@
+function itemTemplate(item){
+    return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    <span class="item-text">${item.text}</span>
+    <div>
+      <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+      <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+    </div>
+  </li>`
+}
+
+//initial page load render
+let ourHTML = items.map(function(item) {
+    return itemTemplate(item)
+}).join('')
+document.getElementById("item-list").insertAdjacentHTML("beforeend", ourHTML)
+
+//create feature
+let createField= document.getElementById("create-field")
+document.getElementById("create-form").addEventListener("submit",function(e){
+    e.preventDefault()
+    axios.post('/create-item',{text: createField.value}).then(function(response){
+        // create html for new element
+        document.getElementById("item-list").insertAdjacentHTML("beforeend",itemTemplate(response.data))
+        createField.value=""
+        createField.focus()
+    }).catch(function(){
+        console.log("try again later")
+    })
+
+})
+
+document.addEventListener("click",function(e){
+   //delete feature
+   if(e.target.classList.contains("delete-me")) {
+       if(confirm("Do you really want to delete")){
+        axios.post('/delete-item',{id: e.target.getAttribute("data-id")}).then(function(){
+            e.target.parentElement.parentElement.remove()
+        }).catch(function(){
+            console.log("try again later")
+        })
+       }
+   }
+
+   
+    //update feature
+    if(e.target.classList.contains("edit-me")) {
+        let userinput = prompt("Enter your desired new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
+        if(userinput){
+            axios.post('/update-item',{text: userinput, id: e.target.getAttribute("data-id")}).then(function(){
+                e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userinput
+            }).catch(function(){
+                console.log("try again later")
+            })
+        }
+    }
+
+})
